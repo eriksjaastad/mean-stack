661fa99a6f4efa731431ef64fd834eeab1cfcf9e
Erik Sjaastad <spudlogic@gmail.com>
2015-03-13 07:47:14 -0700

    added all the files I thought I added before

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser');
 
 //set Node enviornment
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'; 
 
 var app =  express();
 
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
+//Config
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+));
+//static rout handeling
+app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function(req, res) {
+	res.render('partials/' + req.params.partialPath);
+});
 
 app.get('*', function(req, res) {
 	res.render('index');
