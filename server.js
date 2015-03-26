39cfa0336c15ef80dab8cb5311acc9ba1b49682c
Erik Sjaastad <spudlogic@gmail.com>
2015-03-26 07:52:00 -0700

    Got mongodb to work

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
 	console.log('mean-stack, db opened');
 });
-// var messageSchema = mongoose.Schema({message: String});
-// var Message = mongoose.model('Message', messageSchema);
-// var mongoMessage;
-// Message.findOne().exec(function(err, messageDoc) {
-// 	mongoMessage = messageDoc.message;
-// });
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+	mongoMessage = messageDoc.message;
+});
 
 
 app.get('/partials/:partialPath', function(req, res) {
