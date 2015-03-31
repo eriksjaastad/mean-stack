61b62f8874a7f5f85bc5c1a9bd45e5229faa7ca6
Erik Sjaastad <spudlogic@gmail.com>
2015-03-31 10:20:14 -0700

    added sample data and broke the main partial in to featured and new courses

--- server.js
+++ server.js
@@ -43,12 +43,6 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
 	console.log('mean-stack, db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-	mongoMessage = messageDoc.message;
-});
 
 
 app.get('/partials/:partialPath', function(req, res) {
@@ -56,9 +50,7 @@ app.get('/partials/:partialPath', function(req, res) {
 });
 
 app.get('*', function(req, res) {
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = process.env.PORT || 3030;
