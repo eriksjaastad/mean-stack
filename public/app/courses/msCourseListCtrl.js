e137e28c4820bc7f3f1a5e181eec333b80d1ca49
Erik Sjaastad <spudlogic@gmail.com>
2015-04-22 18:15:06 -0700

    added sorting and filtering to course list

--- public/app/courses/msCourseListCtrl.js
+++ public/app/courses/msCourseListCtrl.js
@@ -1,3 +1,9 @@
 angular.module('app').controller('msCourseListCtrl', function($scope, msCourse) {
 	$scope.courses = msCourse.query();
+
+	$scope.sortOptions = [{value:"title", text:"Sort by Title"},
+		{value:"published", text:"Sort by Published Date"}];
+	$scope.sortOrder = $scope.sortOptions[0].value;
+	console.log($scope.sortOptions[0]);
+	console.log($scope.sortOrder);
 });
